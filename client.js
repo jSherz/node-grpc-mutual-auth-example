e4c94e7bfeaee8747f63f63b558698e3712661f0
James Sherwood-Jones <james.sherwoodjones@gmail.com>
2019-02-07 22:43:54 +0000

    Add missing package.json and update for latest GRPC.

--- client.js
+++ client.js
@@ -1,33 +1,43 @@
 const fs = require("fs");
 const path = require("path");
+const protoLoader = require("@grpc/proto-loader");
 const grpc = require("grpc");
 
-const hello_proto = grpc.load('helloworld.proto').helloworld;
+const packageDefinition = protoLoader.loadSync("helloworld.proto");
+const hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
 
 function main() {
-    let client;
+  let client;
 
-    if (process.env.NODE_ENV === "production") {
-        client = new hello_proto.Greeter('localhost:50051', grpc.credentials.createSsl(
-            fs.readFileSync(path.join(process.cwd(), "client-certs", "Snazzy_Microservices.crt")),
-            fs.readFileSync(path.join(process.cwd(), "client-certs", "client-1010101.key")),
-            fs.readFileSync(path.join(process.cwd(), "client-certs", "client-1010101.crt"))
-        ));
-    } else {
-        client = new hello_proto.Greeter('localhost:50051', grpc.credentials.createInsecure());
-    }
+  if (process.env.NODE_ENV === "production") {
+    const certsDir = path.join(process.cwd(), "client-certs");
 
-    client.sayHello({name: 'you'}, function(err, response) {
-        err && console.error(err);
+    client = new hello_proto.Greeter(
+      "localhost:50051",
+      grpc.credentials.createSsl(
+        [fs.readFileSync(path.join(certsDir, "Snazzy_Microservices.crt"))],
+        fs.readFileSync(path.join(certsDir, "client-1010101.key")),
+        fs.readFileSync(path.join(certsDir, "client-1010101.crt"))
+      )
+    );
+  } else {
+    client = new hello_proto.Greeter(
+      "localhost:50051",
+      grpc.credentials.createInsecure()
+    );
+  }
 
-        console.log('Greeting:', response.message);
-    });
+  client.sayHello({ name: "you" }, function(err, response) {
+    err && console.error(err);
 
-    client.sayHelloAgain({name: 'you'}, function(err, response) {
-        err && console.error(err);
+    console.log("Greeting:", response.message);
+  });
 
-        console.log('Greeting:', response.message);
-    });
+  client.sayHelloAgain({ name: "you" }, function(err, response) {
+    err && console.error(err);
+
+    console.log("Greeting:", response.message);
+  });
 }
 
 main();
--- server.js
+++ server.js
@@ -1,36 +1,62 @@
 const fs = require("fs");
 const path = require("path");
+const protoLoader = require("@grpc/proto-loader");
 const grpc = require("grpc");
 
-const hello_proto = grpc.load('helloworld.proto').helloworld;
+const packageDefinition = protoLoader.loadSync("helloworld.proto");
+const hello_proto = grpc.loadPackageDefinition(packageDefinition).helloworld;
 
 function sayHello(call, callback) {
-    callback(null, {message: 'Hello ' + call.request.name});
+  callback(null, { message: "Hello " + call.request.name });
 }
 
 function sayHelloAgain(call, callback) {
-    callback(null, {message: 'Hello again, ' + call.request.name});
+  callback(null, { message: "Hello again, " + call.request.name });
 }
 
 function main() {
-    var server = new grpc.Server();
-    server.addService(hello_proto.Greeter.service, {sayHello: sayHello, sayHelloAgain: sayHelloAgain});
+  var server = new grpc.Server();
+  server.addService(hello_proto.Greeter.service, {
+    sayHello: sayHello,
+    sayHelloAgain: sayHelloAgain
+  });
 
-    if (process.env.NODE_ENV === "production") {
-        server.bind('0.0.0.0:50051', grpc.ServerCredentials.createSsl({
-            rootCerts: fs.readFileSync(path.join(process.cwd(), "server-certs", "Snazzy_Microservices.crt")),
-            keyCertPairs: {
-                privateKey: fs.readFileSync(path.join(process.cwd(), "server-certs", "login.services.widgets.inc.key")),
-                certChain: fs.readFileSync(path.join(process.cwd(), "server-certs", "login.services.widgets.inc.crt"))
-            },
-            checkClientCertificate: true
-        }));
-    } else {
-        server.bind('0.0.0.0:50051', grpc.ServerCredentials.createInsecure());
-    }
+  const certsDir = path.join(process.cwd(), "server-certs");
 
-    server.start();
-    console.log("Server started");
+  if (process.env.NODE_ENV === "production") {
+    server.bind(
+      "0.0.0.0:50051",
+      grpc.ServerCredentials.createSsl({
+        rootCerts: fs.readFileSync(
+          path.join(certsDir, "Snazzy_Microservices.crt")
+        ),
+        keyCertPairs: [
+          {
+            privateKey: fs.readFileSync(
+              path.join(
+                certsDir,
+                "server-certs",
+                "login.services.widgets.inc.key"
+              )
+            ),
+            certChain: fs.readFileSync(
+              path.join(
+                certsDir,
+                "server-certs",
+                "login.services.widgets.inc.crt"
+              )
+            )
+          }
+        ],
+        checkClientCertificate: true
+      })
+    );
+  } else {
+    server.bind("0.0.0.0:50051", grpc.ServerCredentials.createInsecure());
+  }
+
+  server.start();
+  console.log("Server started");
 }
 
 main();
